9f1fe7befa22555e170cc95c0d240987b05947d3
Billy <whorowitz2@gmail.com>
2023-10-09 14:23:38 -0400

    Set up main function and began setting up the menu for the user

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+# Creator: William Horowitz
+# Date: 10/09/2023
+# Description: This program will prompt a user to enter a mathematical operation then ask for two numbers to perform
+#              the operation on
+
+
+# Main Function
+def main():
+
+    print("----------\nCALCULATOR\n---------- ")
+
+
+
+# Call main function
+if __name__ == "__main__":
+    main()
